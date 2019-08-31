142ea8f58446692985bf78a65e2b849ddcb71eb9
Samuel B. Johnson <sabjonso@yahoo.com>
2019-08-31 08:36:33 -0500

    Moving configuration template to toplevel

---
+++ cmake/function_utility_deps.cmake
@@ -0,0 +1,8 @@
+find_package( Git REQUIRED )
+
+execute_process(
+  COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+  WORKING_DIRECTORY ${TYPE_UTILITY_SOURCE_DIR})
+
+list( INSERT CMAKE_MODULE_PATH 0 ${TYPE_UTILITY_SOURCE_DIR}/cmake_modules )
+
