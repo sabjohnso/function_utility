b56cc78e2b4701fba5cacd7f23e33da1b9910f34
Samuel B. Johnson <sabjohnso@yahoo.com>
2021-04-24 16:57:26 -0500

    Using cmake_utilities for testing

--- cmake/function_utility_deps.cmake
+++ cmake/function_utility_deps.cmake
@@ -1,13 +1,14 @@
-#
-# ... Git submodules
-#
-find_package( Git REQUIRED )
-
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
-  WORKING_DIRECTORY ${FUNCTION_UTILITY_SOURCE_DIR})
-
-list( INSERT CMAKE_MODULE_PATH 0 ${FUNCTION_UTILITY_SOURCE_DIR}/cmake_modules )
+if(NOT EXISTS ${PROJECT_SOURCE_DIR}/cmake_utilities/FindCMakeUtilities.cmake)
+  #
+  # ... Git submodules
+  #
+  find_package(Git REQUIRED)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+    WORKING_DIRECTORY ${FUNCTION_UTILITY_SOURCE_DIR})
+endif()
+list( INSERT CMAKE_MODULE_PATH 0 ${FUNCTION_UTILITY_SOURCE_DIR}/cmake_utilities )
+find_package(CMakeUtilities REQUIRED)
 
 
 #
